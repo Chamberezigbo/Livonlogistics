c2b1f770d4446055ecc1e452135023ccccce1b30
chamberezigbo <chamberezigbo@gmail.com>
2024-07-01 09:55:15 +0100

    first commit v1

---
+++ price.php
@@ -0,0 +1,152 @@
+<?php
+include "header.php";
+?>
+
+
+<!-- Header Start -->
+<div class="jumbotron jumbotron-fluid mb-5">
+     <div class="container text-center py-5">
+          <h1 class="text-white display-3">Price</h1>
+          <div class="d-inline-flex align-items-center text-white">
+               <p class="m-0"><a class="text-white" href="">Home</a></p>
+               <i class="fa fa-circle px-3"></i>
+               <p class="m-0">Price</p>
+          </div>
+     </div>
+</div>
+<!-- Header End -->
+
+
+<!-- Pricing Plan Start -->
+<div class="container-fluid pt-5">
+     <div class="container text-center">
+          <div class="pb-2">
+               <h6 class="text-primary text-uppercase font-weight-bold">Pricing Plan</h6>
+               <h1 class="mb-4">Affordable Pricing Packages</h1>
+          </div>
+          <div class="row justify-content-center">
+               <div class="col-md-4 mb-5">
+                    <div class="bg-secondary">
+                         <div class="text-center p-4">
+                              <h1 class="display-4 mb-0">
+                                   <small class="align-top text-muted font-weight-medium" style="font-size: 22px; line-height: 45px;">$</small>550<small class="align-bottom text-muted font-weight-medium" style="font-size: 16px; line-height: 40px;">/72hrs</small>
+                              </h1>
+                         </div>
+                         <div class="bg-primary text-center p-4">
+                              <h3 class="m-0">International</h3>
+                         </div>
+                         <div class="d-flex flex-column align-items-center py-4">
+                              <p>Cutting-edge Technology</p>
+                              <p>Customer-Centric Approach</p>
+                              <p>Warehousing</p>
+                              <p>Express Shipping</p>
+                              <a href="about.php" class="btn btn-primary py-2 px-4 my-2">Order Now</a>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 mb-5">
+                    <div class="bg-secondary">
+                         <div class="text-center p-4">
+                              <h1 class="display-4 mb-0">
+                                   <small class="align-top text-muted font-weight-medium" style="font-size: 22px; line-height: 45px;">$</small>150<small class="align-bottom text-muted font-weight-medium" style="font-size: 16px; line-height: 40px;">/24hrs</small>
+                              </h1>
+                         </div>
+                         <div class="bg-primary text-center p-4">
+                              <h3 class="m-0">Domestic</h3>
+                         </div>
+                         <div class="d-flex flex-column align-items-center py-4">
+                              <p>Insurance</p>
+                              <p>Customer-Centric Approach:</p>
+                              <p>Warehousing</p>
+                              <p>Express Shipping</p>
+                              <a href="about.php" class="btn btn-primary py-2 px-4 my-2">Order Now</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
+<!-- Pricing Plan End -->
+
+
+<!-- Testimonial Start -->
+<div class="container-fluid py-5">
+     <div class="container">
+          <div class="text-center pb-2">
+               <h6 class="text-primary text-uppercase font-weight-bold">Testimonial</h6>
+               <h1 class="mb-4">Our Clients Say</h1>
+          </div>
+          <div class="owl-carousel testimonial-carousel">
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-1.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   Alex Johnson
+                              </h6>
+                              <small>- SwiftTech Industries</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Fast Express Logistics has truly transformed the way we manage our shipping needs.
+                         Their reliable services and efficient solutions have saved us both time and resources.
+                         From the moment we engaged with them, we knew we had found a partner we could trust.
+                    </p>
+               </div>
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-2.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   Sofia García
+                              </h6>
+                              <small>- Nature's Harvest</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Exceptional service and seamless deliveries - that's what Fast Express Logistics delivers every time.
+                         Their attention to detail and commitment to meeting deadlines have consistently exceeded our expectations.
+                         It's been a pleasure working with a team that values our business as much as we do.
+                    </p>
+               </div>
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-3.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   James Anderson
+                              </h6>
+                              <small>- TechLink Innovations</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Fast Express Logistics is more than a logistics provider; they're a strategic partner. Their tailored solutions have optimized our supply chain,
+                         enabling us to focus on our core business. The team's expertise and dedication have contributed to our success in more ways than one.
+                    </p>
+               </div>
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-4.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   Lara Müller
+                              </h6>
+                              <small>- Trendy Treasures</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Reliability, professionalism, and speed - that's what Fast Express Logistics represents. Their end-to-end logistics support has been instrumental in our growth journey. As a customer-oriented company,
+                         they've consistently gone above and beyond to ensure our shipments arrive on time and in perfect condition.
+                    </p>
+               </div>
+          </div>
+     </div>
+</div>
+<!-- Testimonial End -->
+
+<?php
+include "footer.php";
